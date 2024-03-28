bfd41ee5b575a97f87077edcfdd414b7afcf5146
Steven Dong <stevendong@microsoft.com>
2024-03-28 14:37:14 -0400

    improvemrnts

--- kafka/producer/main.go
+++ kafka/producer/main.go
@@ -7,11 +7,11 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	crand "crypto/rand"
@@ -29,97 +29,92 @@ const eventHubNamespace = "EVENTHUB_NAMESPACE"
 const eventHub = "EVENTHUB"
 const msg = "MSG"
 
+var eventId = 0
+var logLocation = GetEnv("LOG_FILE")
+
 func main() {
+	if len(logLocation) > 0 {
+		f, err := os.OpenFile(logLocation, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0740)
+		if err != nil {
+			log.Printf("Unable to open file log location: %s", err.Error())
+		}
+		log.SetOutput(f)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 
 	credential, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
-		log.Println(err.Error())
+		log.Printf("Retrieving Azure Credential failed: %s", err.Error())
+		os.Exit(1)
 	}
 
 	eventHubNamespace := fmt.Sprintf(
 		"%s.servicebus.windows.net",
-		getEnv(eventHubNamespace))
+		GetEnv(eventHubNamespace))
 
 	// Event Hubs producer
 	producerClient, err := azeventhubs.NewProducerClient(
 		eventHubNamespace,
-		getEnv(eventHub),
+		GetEnv(eventHub),
 		credential,
 		nil)
 
 	if err != nil {
-		log.Println(err.Error())
+		log.Printf("Creating Producer Client failed: %s", err.Error())
+		os.Exit(1)
 	}
 
 	defer producerClient.Close(context.Background())
 	for {
 		events := createEventsForDemo()
-
 		newBatchOptions := &azeventhubs.EventDataBatchOptions{}
 		// Creates an EventDataBatch, which you can use to pack multiple events together, allowing for efficient transfer.
 		batch, err := producerClient.NewEventDataBatch(context.Background(), newBatchOptions)
 		if err != nil {
-			panic(err)
+			log.Printf("Creating event batch failed: %s", err.Error())
+			os.Exit(1)
 		}
 
-		for i := 0; i < len(events); i++ {
-			err = batch.AddEventData(events[i], nil)
-
-			if errors.Is(err, azeventhubs.ErrEventDataTooLarge) {
-				if batch.NumEvents() == 0 {
-					// This one event is too large for this batch, even on its own. No matter what we do it
-					// will not be sendable at its current size.
-					log.Println("The single event is too large to be sent.")
-				}
-
-				// This batch is full - we can send it and create a new one and continue
-				// packaging and sending events.
-				if err := producerClient.SendEventDataBatch(context.Background(), batch, nil); err != nil {
-					log.Printf("Event sending failed %s", err.Error())
-				}
-
-				// create the next batch we'll use for events, ensuring that we use the same options
-				// each time so all the messages go the same target.
-				tmpBatch, err := producerClient.NewEventDataBatch(context.Background(), newBatchOptions)
-
-				if err != nil {
-					log.Printf("Creating new batch failed: %s", err.Error())
-				}
-
-				batch = tmpBatch
-
-				// rewind so we can retry adding this event to a batch
-				i--
-			} else if err != nil {
-				log.Printf("Errored while adding events to batch: %s", err.Error())
-			}
+		err = batch.AddEventData(events[0], nil)
+		if err != nil {
+			log.Printf("Adding event data to batch failed: %s", err.Error())
 		}
 
-		// if we have any events in the last batch, send it
-		if batch.NumEvents() > 0 {
-			log.Println("sending events")
-			if err := producerClient.SendEventDataBatch(context.Background(), batch, nil); err != nil {
-				log.Printf("Error sending events: %s", err.Error())
-			}
+		if err := producerClient.SendEventDataBatch(context.Background(), batch, nil); err != nil {
+			log.Printf("Event sending failed %s", err.Error())
 		}
-		log.Println("Rest for 10 seconds before sending additional events.")
+
+		select {
+		case sig := <-signals:
+			log.Printf("Got signal: %v", sig)
+			return
+		default:
+		}
+
 		time.Sleep(time.Second * 1)
 	}
 }
 
+func GetEnv(envName string) string {
+	value, exists := os.LookupEnv(envName)
+	if !exists {
+		log.Println("Environment variable '" + envName + "' is not set.")
+		os.Exit(1)
+	}
+	return value
+}
+
 func createEventsForDemo() []*azeventhubs.EventData {
-	rand.Seed(time.Now().UnixNano())
-	randomId := rand.Intn(90000) + 10000
-	rawMessage := getEnv(msg)
-	value := fmt.Sprintf("Message Id %d: %s", randomId, rawMessage)
+	eventId += 1
+	rawMessage := GetEnv(msg)
+	value := fmt.Sprintf("Message Id %d: %s", eventId, rawMessage)
 	encryptedValue, err := encryptMessage(value, "path_to_public_key.pem")
 	if err != nil {
 		log.Println("Encrypting message failed.")
 	}
 	return []*azeventhubs.EventData{
-		{
-			Body: []byte(encryptedValue),
-		},
 		{
 			Body: []byte(encryptedValue),
 		},
@@ -129,7 +124,7 @@ func createEventsForDemo() []*azeventhubs.EventData {
 func encryptMessage(plaintext string, publicKeyPath string) (string, error) {
 	var pubpem []byte
 	var err error
-	if pkey := getEnv("PUBKEY"); len(pkey) > 0 {
+	if pkey := GetEnv("PUBKEY"); len(pkey) > 0 {
 		pubpem = []byte(pkey)
 	}
 	if len(pubpem) == 0 {
@@ -155,13 +150,4 @@ func encryptMessage(plaintext string, publicKeyPath string) (string, error) {
 	}
 
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
-}
-
-func getEnv(envName string) string {
-	value := os.Getenv(envName)
-	if value == "" {
-		fmt.Println("Environment variable '" + envName + "' is missing")
-		os.Exit(1)
-	}
-	return value
 }
